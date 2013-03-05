5ed78c6c496fae90d5d0c75dc081370c29b0547f
Sean Stewart <sdstewar@gmail.com>
2013-03-05 16:22:55 -0500

    Update Configuration.php

--- LDAP/LDAPAuthBundle/DependencyInjection/Configuration.php
+++ LDAP/LDAPAuthBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('eiorldap_auth');
+        $rootNode = $treeBuilder->root('ldapldap_auth');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
